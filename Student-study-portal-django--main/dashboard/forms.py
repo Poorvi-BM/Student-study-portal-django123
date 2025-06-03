5abdb36af283304d1bdd906eae64af1f663a156e
Preranans10 <147045147+Preranans10@users.noreply.github.com>
2025-06-03 12:24:26 +0530

    updates urls.py for youtube search

---
+++ Student-study-portal-django--main/dashboard/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'description']
+        
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+        
+class HomeworkForm(forms.ModelForm):
+    class Meta:
+        model = Homework
+        widgets = {'due':DateInput()}
+        fields = ['subject', 'title', 'description', 'due', 'is_finished']
+        
+
+class DashboardForm(forms.Form):
+ 
+    text = forms.CharField(label='Search',max_length=100)
+    
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'is_finished']
+        
+        
+        
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        
